2d23c6a5c19b7f7e420425256030f5606373620c
Valdemar22848 <122788990+Valdemar22848@users.noreply.github.com>
2024-02-24 11:10:05 +0300

    Work_3_1
    Старая формула

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Work_3;
+public class Class1
+{
+    public void Main ()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = 5 * x + 20;
+        Console.WriteLine(y);
+    }
+}
